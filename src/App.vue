8ed5528ed41dbdf12fce17dd4ad6875d8b12175f
Antonbinom <protest_the_hero@mail.ru>
2024-06-26 12:04:54 +0300

    add: test components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script setup>
-import { Alert } from "vue-tg";
+import { Alert, BackButton, ClosingConfirmation, Confirm, Popup } from "vue-tg";
+const isBackButtonVisible = ref(true);
+const isAlert = ref(false);
+
+const handleBackButton = () => {
+  isBackButtonVisible.value = false;
+  setTimeout(() => {
+    isBackButtonVisible.value = true;
+  }, 5000);
+};
+
+const handleConfirmClose = (ok) => {
+  isAlert.value = ok;
+};
+function handlePopupClose(buttonId) {
+  console.log(buttonId);
+}
 </script>
 
 <template>
   <main>
-    <Alert message="Hello! I am telegram mini app." />
+    <Alert v-if="isAlert" message="Hello! I am telegram mini app." />
+    <BackButton :visible="isBackButtonVisible" @click="handleBackButton" />
+    <ClosingConfirmation />
+    <Confirm message="Hello?" @close="handleConfirmClose" />
+    <Popup
+      message="Hello, i am popup"
+      title="Popup"
+      @close="handlePopupClose"
+    />
   </main>
 </template>
